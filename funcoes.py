e63492dd719cbbc6b75d78c30ae9159d48f85f21
Matheus Wielevski <matheusprogramming083@gmail.com>
2026-06-02 17:14:24 -0300

    Impede cadastro com email duplicado

--- funcoes.py
+++ funcoes.py
@@ -1,19 +1,32 @@
 from dados import usuarios
 
 def cadastrar_usuario():
-    nome = input("Digite o nome do usuário: ")
-    email = input("Digite o email do usuário: ")
-    senha = input("Digite a senha do usuário: ")
 
-    usuario = {
-        "nome": nome,
-        "email": email,
-        "senha": senha,
-        "saldo": 0
-    }
+    while True:
+        nome = input("Digite o nome do usuário: ")
+        email = input("Digite o email do usuário: ")
 
-    usuarios.append(usuario)
-    print(f"Usuário cadastrado com sucesso! Bem-vindo, {nome}!")
+        email_repetido = False
+
+        for usuario in usuarios:
+
+            if email == usuario["email"]:
+                print("Email já cadastrado! Tente novamente")
+                email_repetido = True
+        
+        if email_repetido == False:
+            senha = input("Digite a senha do usuário: ")
+
+            usuario = {
+                "nome": nome,
+                "email": email,
+                "senha": senha,
+                "saldo": 0
+            }
+
+            usuarios.append(usuario)
+            print(f"Usuário cadastrado com sucesso! Bem-vindo, {nome}!")
+            break
 
 def logar_usuario():
     
