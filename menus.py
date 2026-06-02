f4933d3b0bc1aae37d30d9079243dfc4ec3a4884
Matheus Wielevski <matheusprogramming083@gmail.com>
2026-06-02 16:27:02 -0300

    Impede valores inválidos em operações bancárias

--- menus.py
+++ menus.py
@@ -36,15 +36,22 @@ def menu_conta(usuario_logado):
             print(f"R${usuario_logado['saldo']}")
         elif escolha_usuario == 2:
             deposito = int(input("Insira o valor do depósito: "))
-            usuario_logado['saldo'] += deposito
-            print(f"Depositado com sucesso! Seu saldo agora é de: R${usuario_logado['saldo']}")
+            if deposito > 0:
+                usuario_logado['saldo'] += deposito
+                print(f"Depositado com sucesso! Seu saldo agora é de: R${usuario_logado['saldo']}")
+            else:
+                print("Valor inválido! Digite um valor maior que zero.")
+
         elif escolha_usuario == 3:
             saque = int(input("Insira o valor do saque: "))
-            if saque > usuario_logado['saldo']:
-                print("Saldo insuficiente")
-            elif saque <= usuario_logado['saldo']:
-                usuario_logado['saldo'] -= saque
-                print(f"Saque realizado com sucesso! Seu saldo agora é de: R${usuario_logado['saldo']}")
+            if saque > 0:
+                if saque > usuario_logado['saldo']:
+                    print("Saldo insuficiente")
+                elif saque <= usuario_logado['saldo']:
+                    usuario_logado['saldo'] -= saque
+                    print(f"Saque realizado com sucesso! Seu saldo agora é de: R${usuario_logado['saldo']}")
+            else:
+                print("Valor inválido! Digite um valor maior que zero.")
         elif escolha_usuario == 4:
             print("Saindo do menu da conta. Até mais!")
             break
