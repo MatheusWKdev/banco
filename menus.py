d8eaffecd60f79b210e8a6d379e7c259d3e08bdd
Matheus Wielevski <matheusprogramming083@gmail.com>
2026-06-01 21:11:24 -0300

    Implementa depósito e saque

--- menus.py
+++ menus.py
@@ -11,10 +11,37 @@ def menu_principal():
         print(" 3 - sair do programa")
     
         escolha_usuario = int(input("Digite o número da opção desejada: "))
+
         if escolha_usuario == 1:
             cadastrar_usuario()
         elif escolha_usuario == 2:
-            logar_usuario()
+            usuario_logado = logar_usuario()
+            menu_conta(usuario_logado)
+            break
         elif escolha_usuario == 3:
             print("Saindo do programa. Até mais!")
             break
+
+def menu_conta(usuario_logado):
+    while True:
+        print("=== Conta Bancária ===")
+        print("1 - Ver saldo")
+        print("2 - Depositar")
+        print("3 - Sacar")
+        print("4 - Logout")
+        
+        escolha_usuario = int(input("Digite o número da opção desejada: "))
+
+        if escolha_usuario == 1:
+            print(f"R${usuario_logado['saldo']}")
+        elif escolha_usuario == 2:
+            deposito = int(input("Insira o valor do depósito: "))
+            usuario_logado['saldo'] += deposito
+            print(f"Depositado com sucesso! Seu saldo agora é de: R${usuario_logado['saldo']}")
+        elif escolha_usuario == 3:
+            saque = int(input("Insira o valor do saque: "))
+            if saque > usuario_logado['saldo']:
+                print("Saldo insuficiente")
+            elif saque <= usuario_logado['saldo']:
+                usuario_logado['saldo'] -= saque
+                print(f"Saque realizado com sucesso! Seu saldo agora é de: R${usuario_logado['saldo']}")
